a98d7063dfd4e9004be8b704b123c7276ac7e876
thejayadad <thejayadad@gmail.com>
2023-09-22 09:11:09 -0400

    update

--- myapp/components/Menu.js
+++ myapp/components/Menu.js
@@ -1,19 +1,24 @@
-import React from 'react'
+import React from 'react';
+import Link from 'next/link'; // Import Link from Next.js
 
-const data = [
-    {id: 1, name: "Home", url:"/"},
-    {id: 2, name: "About", url: "/about"},
-    {id: 3, name: "Contact", url: "/contact"}
-]
+const links = [
+  { id: 1, name: 'Home', url: '/' },
+  { id: 2, name: 'About', url: '/about' },
+  { id: 3, name: 'Contact', url: '/contact' },
+];
 
 const Menu = () => {
   return (
-    <ul
-    className='hidden md:flex items-center gap-8 font-medium text-black'
-    >
-
+    <ul className='hidden md:flex items-center gap-8 font-medium text-black'>
+      {links.map((link) => (
+        <li key={link.id}>
+          <Link href={link.url}>
+            <a>{link.name}</a>
+          </Link>
+        </li>
+      ))}
     </ul>
-  )
-}
+  );
+};
 
-export default Menu
+export default Menu;
